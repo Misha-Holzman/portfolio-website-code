05a6e6a78606d823fd60196ed39ef1f1a6c897dc
Misha Holzman <mcholzman1@gmail.com>
2021-06-17 11:26:45 -0400

    added new graphic

--- src/GraphicDesign/index.js
+++ src/GraphicDesign/index.js
@@ -9,7 +9,7 @@ import cudaLogo4 from '../assets/cuda-logo-color.jpg'
 import surferGraphic from '../assets/the-surfer.png'
 import wavyColorGraphic from '../assets/wavy-color-graphic.png'
 import WhaleGraphic from '../assets/whale-geometry.png'
-// import NumberGraphic from '../assets/3d-number-two-ocean.png'
+import colorWaveGraphic from '../assets/color-wave.png'
 import NumberGraphic from '../assets/3d-twenty-two-ocean-pant.png'
 import GeometricShapes from '../assets/geometric-shapes.png'
 
@@ -18,8 +18,9 @@ class GraphicDesign extends Component {
     return (
       <div className='graphic-design-page'>
         <div className='graphic-designs-img-div'>
-          <img src={wavyColorGraphic} alt='' className='wavy-color-graphic-img' />
           <img src={GeometricShapes} alt='' className='geo-shapes-graphic-img' />
+          <img src={wavyColorGraphic} alt='' className='wavy-color-graphic-img' />
+          <img src={colorWaveGraphic} alt='' className='color-wave-graphic-img' />
           <img src={surferGraphic} alt='' className='surfer-graphic-img' />
           <img src={WhaleGraphic} alt='' className='whale-graphic-img' />
           <img src={NumberGraphic} alt='' className='number-graphic-img' />
